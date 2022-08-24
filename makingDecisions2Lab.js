478511d77a6d040eb260ccee8252375762658264
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2022-08-23 18:36:38 -0600

    Completed 'problems' 6-11

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -94,7 +94,7 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
-
+let someNum = myFavoriteNumbers[4];
 
 ////////// PROBLEM 7 //////////
 
@@ -102,7 +102,12 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-
+if (myFavoriteNumbers.length < 7) {
+  console.log("There are not enough elements in this array");
+}
+else {
+  someNum = myFavoriteNumbers[6]
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -113,7 +118,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for (i = 0; i < listOfNumbers.length; i++){
+  if (listOfNumbers[i] % 3 === 0) {
+    console.log(`${listOfNumbers[i]} is divisible by 3`);
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -125,7 +134,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (i = letters.length - 1; i > -1; i--) {
+  console.log(letters[i]);
+}
 
 ////////// Advanced Problems //////////
 
@@ -147,7 +158,24 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-
+if (letterGrade === 'A') {
+  console.log("The student is doing excellently.");
+}
+else if (letterGrade === 'B') {
+  console.log("The student is doing well.");
+}
+else if (letterGrade === 'C') {
+  console.log("The student is doing alright.");
+}
+else if (letterGrade === 'D') {
+  console.log("The student is not doing very well.");
+}
+else if (letterGrade === 'F') {
+  console.log("The student is failing.");
+}
+else {
+  console.log("Not an eligible grade.");
+}
 
 
 ////////// PROBLEM 11 //////////
@@ -175,3 +203,17 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for (i = 1; i <= 100; i++) {
+  if (i % 15 === 0) {
+    console.log("DevMountain");
+  }
+  else if (i % 3 === 0) {
+    console.log("Dev");
+  }
+  else if (i % 5 === 0) {
+    console.log("Mountain");
+  }
+  else {
+    console.log(i);
+  }
+}
